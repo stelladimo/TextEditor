b71bdb38d3c1ade4420208920ec1fd807fc54297
stelladimo <stella.demopoulou@gmail.com>
2024-05-27 23:13:03 +0300

    final touches

---
+++ source/repos/TextEditor/TextEditor/Data/TextEditorDB.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TextEditor.Models;
+
+namespace TextEditor.Data
+{
+    public class TextEditorDB : IdentityDbContext
+    {
+        public TextEditorDB(DbContextOptions<TextEditorDB> options)
+            : base(options)
+        {
+        }
+        public DbSet<Doc> Docs { get; set; }
+    }
+}
---
+++ source/repos/TextEditor/TextEditor/Program.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using TextEditor.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<TextEditorDB>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddEntityFrameworkStores<TextEditorDB>();
+builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<TextEditorDB>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Docs}/{action=Index}/{id?}");
+app.MapRazorPages();
+
+app.Run();
